1e320b3e010e370eb0599d68f47960f597691626
ztaylortyson <ztaylortyson@yahoo.com>
2023-02-06 10:12:26 -0800

    buymeacoffee.com

--- app/controllers/concerns/trial_calendars.rb
+++ app/controllers/concerns/trial_calendars.rb
@@ -3,6 +3,7 @@ require 'open-uri'
 module TrialCalendars
     
     def launch
+        @arry = []
         @myhash = {}
         get_svcal1
         get_caldate
@@ -20,21 +21,17 @@ module TrialCalendars
     end
 
     def scrape
-        @arry = []
         @page.xpath('//tr[@class="MYDETAILTABLE"]').each do |i|
-            if i.inner_html.include?('Civil Court Trial') # Civil Jury Trial
-                 @myhash[:trial_name] = i.css('td')[3].text
-                 @myhash[:trial_number] = i.css('td')[1].text
-                 @myhash[:trial_time] = i.css('td')[0].text
-                 @myhash[:type] = "Court Trial"
-            elsif i.inner_html.include?('Civil Jury Trial')
-                 @myhash[:trial_name] = i.css('td')[3].text
-                 @myhash[:trial_number] = i.css('td')[1].text
-                 @myhash[:trial_time] = i.css('td')[0].text
-                 @myhash[:type] = "Jury Trial"
+            if i.inner_html.include?('Civil Jury Trial') || i.inner_html.include?('Civil Court Trial')
+                myhash = {}
+                 myhash[:trial_name] = i.css('td')[3].text
+                 myhash[:trial_number] = i.css('td')[1].text
+                 myhash[:trial_time] = i.css('td')[0].text
+                 myhash[:event_time] = i.css('td')[2].text
+                 @arry << myhash
             end
+            # p i.text.strip if i.inner_html.include?('Civil Jury Trial')
         end
-        @arry << @myhash
     end
 
 
